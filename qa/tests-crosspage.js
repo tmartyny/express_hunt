fec5ce4d88bd4f2a9190db027a906c0c4a29587c
Teresa Martyny <teresamartyny@gmail.com>
2015-08-18 23:52:29 -0700

    remove test. browser.field is not an option listed in the zombie docs and is returning null when used. Will troubleshoot and write different tests using docs in later commit.

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -1,23 +1,8 @@
-var Browser = require('zombie'),
-  assert = require('chai').assert;
+var Browser = require('zombie');
+var assert = require('chai').assert;
 
-var browser;
+var browser = new Browser();
 
 suite('Cross Page Tests', function(){
-  setup(function(){
-    browser = new Browser();
-  });
-  // test('Starting the Scavenger Hunt' + 
-  //   ' should populate the referrer field', function(done){
-  //     var referrer = 'http://andre:3000';
-  //     browser.visit(referrer, function(){
-  //         console.log("referrer: " + browser.field('referrer'));
-  //       browser.clickLink('.js_beginHunt', function(){
-  //         console.log('click check');
-          
-  //         assert(browser.field('referrer').value === referrer);
-  //         done();
-  //       });
-  //     });
-  //   });
+
 });
