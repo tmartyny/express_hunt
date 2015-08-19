32985ed6e0ace3e69d12546261f68bc63f11d639
Teresa Martyny <teresamartyny@gmail.com>
2015-08-18 23:21:19 -0700

    add crosspage test file and challenges show page

---
+++ qa/tests-crosspage.js
@@ -0,0 +1,23 @@
+var Browser = require('zombie'),
+  assert = require('chai').assert;
+
+var browser;
+
+suite('Cross Page Tests', function(){
+  setup(function(){
+    browser = new Browser();
+  });
+  test('Starting the Scavenger Hunt' + 
+    ' should populate the referrer field', function(done){
+      var referrer = 'http://andre:3000';
+      browser.visit(referrer, function(){
+          console.log("referrer: " + browser.field('referrer'));
+        browser.clickLink('.js_beginHunt', function(){
+          console.log('click check');
+          
+          assert(browser.field('referrer').value === referrer);
+          done();
+        });
+      });
+    });
+});
